54987b5f59d1c8b2a824726b6667398ab341d689
Volkan Akyel <volkan@uikielle.com>
2024-02-21 17:21:43 +0100

    gamer-form |🔄 update tailwind config, remove purge for content option

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  content: ['./src/**/*.{vue,js,ts,jsx,tsx}', './public/index.html'],
   theme: {
     extend: {
       maxWidth: {
