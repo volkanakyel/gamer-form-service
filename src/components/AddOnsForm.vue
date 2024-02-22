998b4c267af22f18700b3bb37606a978e89cbe76
Volkan Akyel <volkan@uikielle.com>
2024-02-22 13:25:20 +0100

    gamer-form |🔄 clean step component making them lighter

--- src/components/AddOnsForm.vue
+++ src/components/AddOnsForm.vue
@@ -1,84 +1,72 @@
 <template>
-  <div class="step-content">
-    <h2 class="text-4xl mb-3 mt-10 text-l font-bold">Pick add-ons</h2>
-    <p class="text-grey-300">
-      Add-ons help enhance your gaming experience.
-    </p>
-    <div class="flex flex-col gap-4 mt-8">
-      <div class="flex gap-4">
-        <div
-          class="p-4 border bg-grey-200 border-purple rounded-lg flex items-center justify-between max-w-addons w-full">
-          <div class="flex items-center space-x-4">
-            <div class="flex items-center gap-6">
-              <input id="checked-checkbox" type="checkbox" value=""
-                class="w-4 h-4 text-blue-600 bg-purple rounded focus:bg-purple dark:focus:bg-purple dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-grey-300" />
+  <h2 class="text-4xl mb-3 mt-10 text-l font-bold">Pick add-ons</h2>
+  <p class="text-grey-300">
+    Add-ons help enhance your gaming experience.
+  </p>
+  <div class="flex flex-col gap-4 mt-8">
+    <div class="flex gap-4">
+      <div class="p-4 border bg-grey-200 border-purple rounded-lg flex items-center justify-between max-w-addons w-full">
+        <div class="flex items-center space-x-4">
+          <div class="flex items-center gap-6">
+            <input id="checked-checkbox" type="checkbox" value=""
+              class="w-4 h-4 text-blue-600 bg-purple rounded focus:bg-purple dark:focus:bg-purple dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-grey-300" />
 
-              <div>
-                <h4 class="text-s font-semibold">Online service</h4>
-                <p class="text-grey-300 text-base">
-                  Access to multiplayer games
-                </p>
-              </div>
+            <div>
+              <h4 class="text-s font-semibold">Online service</h4>
+              <p class="text-grey-300 text-base">
+                Access to multiplayer games
+              </p>
             </div>
-          </div>
-          <div>
-            <p class="text-blue-500 text-base text-purple font-light">
-              +$1/mo
-            </p>
           </div>
         </div>
-      </div>
-      <div class="flex">
-        <div
-          class="p-4 border bg-grey-200 border-purple rounded-lg flex items-center justify-between max-w-addons w-full">
-          <div class="flex items-center space-x-4">
-            <div class="flex items-center gap-6">
-              <input id="checked-checkbox" type="checkbox" value=""
-                class="w-4 h-4 text-blue-600 bg-purple rounded focus:bg-purple dark:focus:bg-purple dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-grey-300" />
-
-              <div>
-                <h4 class="text-s font-semibold">Larger storage</h4>
-                <p class="text-grey-300 text-base">
-                  Extra 1TB of cloud save
-                </p>
-              </div>
-            </div>
-          </div>
-          <div>
-            <p class="text-purple font-light text-base">+$1/mo</p>
-          </div>
-        </div>
-      </div>
-      <div class="flex">
-        <div
-          class="p-4 border bg-grey-200 border-purple rounded-lg flex items-center justify-between max-w-addons w-full">
-          <div class="flex items-center space-x-4">
-            <div class="flex items-center gap-6">
-              <input id="checked-checkbox" type="checkbox" value=""
-                class="w-4 h-4 text-blue-600 bg-purple rounded focus:bg-purple dark:focus:bg-purple dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-grey-300" />
-
-              <div>
-                <h4 class="text-s font-semibold">Customizable Profile</h4>
-                <p class="text-grey-300 text-base">
-                  Custom theme on your profile
-                </p>
-              </div>
-            </div>
-          </div>
-          <div>
-            <p class="text-blue-500 text-base text-purple font-light">
-              +$1/mo
-            </p>
-          </div>
+        <div>
+          <p class="text-blue-500 text-base text-purple font-light">
+            +$1/mo
+          </p>
         </div>
       </div>
     </div>
-    <div class="flex justify-between mt-20 items-center mb-2">
-      <button class="text-grey-300">Go Back</button>
+    <div class="flex">
+      <div class="p-4 border bg-grey-200 border-purple rounded-lg flex items-center justify-between max-w-addons w-full">
+        <div class="flex items-center space-x-4">
+          <div class="flex items-center gap-6">
+            <input id="checked-checkbox" type="checkbox" value=""
+              class="w-4 h-4 text-blue-600 bg-purple rounded focus:bg-purple dark:focus:bg-purple dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-grey-300" />
 
-      <button class="next-btn">
-        Next Step
-      </button>
+            <div>
+              <h4 class="text-s font-semibold">Larger storage</h4>
+              <p class="text-grey-300 text-base">
+                Extra 1TB of cloud save
+              </p>
+            </div>
+          </div>
+        </div>
+        <div>
+          <p class="text-purple font-light text-base">+$1/mo</p>
+        </div>
+      </div>
+    </div>
+    <div class="flex">
+      <div class="p-4 border bg-grey-200 border-purple rounded-lg flex items-center justify-between max-w-addons w-full">
+        <div class="flex items-center space-x-4">
+          <div class="flex items-center gap-6">
+            <input id="checked-checkbox" type="checkbox" value=""
+              class="w-4 h-4 text-blue-600 bg-purple rounded focus:bg-purple dark:focus:bg-purple dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-grey-300" />
+
+            <div>
+              <h4 class="text-s font-semibold">Customizable Profile</h4>
+              <p class="text-grey-300 text-base">
+                Custom theme on your profile
+              </p>
+            </div>
+          </div>
+        </div>
+        <div>
+          <p class="text-blue-500 text-base text-purple font-light">
+            +$1/mo
+          </p>
+        </div>
+      </div>
     </div>
   </div>
 </template>
--- src/components/PersonalInfoForm.vue
+++ src/components/PersonalInfoForm.vue
@@ -1,39 +1,28 @@
 <template>
-  <div class="step-content">
-    <h2 class="text-4xl mb-3 mt-10 text-l font-bold">Personal info</h2>
-    <p class="text-grey-300">
-      Please provide your name, email address, and phone number.
-    </p>
-    <div class="mt-8">
-      <div class="relative">
-        <label for="name">Name</label>
-        <input type="text" id="name" placeholder="e.g. Stephen King"
-          class="border border-grey-100 rounded pl-4 py-2 w-full focus:outline-none focus:border-gray-500" />
-      </div>
-      <div class="relative mt-6">
-        <label for="name">Email Address</label>
-        <input type="email" id="name" placeholder="e.g. Stephen King"
-          class="border border-grey-100 rounded pl-4 py-2 w-full focus:outline-none focus:border-gray-500" />
-      </div>
-      <div class="relative mt-6">
-        <label for="name">Phone Number</label>
-        <input type="text" id="name" placeholder="e.g. Stephen King"
-          class="border border-grey-100 rounded pl-4 py-2 w-full focus:outline-none focus:border-gray-500" />
-      </div>
+  <h2 class="text-4xl mb-3 mt-10 text-l font-bold">Personal info</h2>
+  <p class="text-grey-300">
+    Please provide your name, email address, and phone number.
+  </p>
+  <div class="mt-8">
+    <div class="relative">
+      <label for="name">Name</label>
+      <input type="text" id="name" placeholder="e.g. Stephen King"
+        class="border border-grey-100 rounded pl-4 py-2 w-full focus:outline-none focus:border-gray-500" />
     </div>
-    <button @click="updateStep" class="next-btn">
-      Next Step
-    </button>
+    <div class="relative mt-6">
+      <label for="name">Email Address</label>
+      <input type="email" id="name" placeholder="e.g. Stephen King"
+        class="border border-grey-100 rounded pl-4 py-2 w-full focus:outline-none focus:border-gray-500" />
+    </div>
+    <div class="relative mt-6">
+      <label for="name">Phone Number</label>
+      <input type="text" id="name" placeholder="e.g. Stephen King"
+        class="border border-grey-100 rounded pl-4 py-2 w-full focus:outline-none focus:border-gray-500" />
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { defineEmits } from 'vue'
-type Emits = (event: 'updateStep') => void
-const emit: Emits = defineEmits(['updateStep'])
-const updateStep = () => {
-  emit('updateStep')
-}
 </script>
 
 <style scoped></style>
