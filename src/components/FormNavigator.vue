7c9b49b60a31ae72badf840a7ecdfa823610c5e6
Volkan Akyel <volkan@uikielle.com>
2024-02-22 13:23:18 +0100

    gamer-form |🚀 add Navigator component to navigate though steps

---
+++ src/components/FormNavigator.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="flex justify-between mt-20 items-center mb-2">
+    <button class="text-grey-300">Go Back</button>
+
+    <button @click="updateStep" class="next-btn">
+      Next Step
+    </button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineEmits } from 'vue'
+type Emits = (event: 'updateStep') => void
+const emit: Emits = defineEmits(['updateStep'])
+const updateStep = () => {
+  emit('updateStep')
+}
+</script>
+
+<style scoped></style>
