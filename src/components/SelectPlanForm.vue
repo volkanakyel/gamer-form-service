71ea6ef0c9a2958068c7d97b35f543692764d787
Volkan Akyel <volkan@uikielle.com>
2024-02-27 13:32:07 +0100

    gamer-form |🎨 Add TogglePlan component for Olan sleection

--- src/components/SelectPlanForm.vue
+++ src/components/SelectPlanForm.vue
@@ -33,22 +33,12 @@
         </div>
       </div>
     </div>
-    <div class="flex justify-center gap-6 mt-8 w-full bg-grey-200 rounded-lg p-3">
-      <p class="font-bold">Monthly</p>
-      <label for="toggleFour" class="flex items-center cursor-pointer select-none text-dark dark:text-white">
-        <div class="relative">
-          <input type="checkbox" id="toggleFour" class="peer sr-only" />
-          <div class="block h-8 rounded-full box bg-dark dark:bg-dark-2 w-14 peer-checked:bg-primary"></div>
-          <div
-            class="absolute flex items-center justify-center w-6 h-6 transition bg-white rounded-full dot left-1 top-1 dark:bg-dark-5 peer-checked:translate-x-full peer-checked:dark:bg-white">
-          </div>
-        </div>
-      </label>
-      <p class="text-grey-300">Yearly</p>
-    </div>
+    <TogglePlan />
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import TogglePlan from './TogglePlan.vue';
+</script>
 
 <style scoped></style>
