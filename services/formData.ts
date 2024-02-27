8074e1b32d80df2da1e3ddc76c57b7765c24ea79
Volkan Akyel <volkan@uikielle.com>
2024-02-27 19:20:11 +0100

    gamer-form |📊 add form data

---
+++ services/formData.ts
@@ -0,0 +1,31 @@
+interface AddOnPlan {
+  title: string;
+  description: string;
+  active: boolean;
+  monthlyPrice: number;
+  yearlyPrice: number;
+}
+
+const addOnsPlan: AddOnPlan[] = [
+  {
+    title: "Online service",
+    description: "Access to multiplayer games",
+    active: false,
+    monthlyPrice: 10,
+    yearlyPrice: 90,
+  },
+  {
+    title: "Larger storage",
+    description: "Extra 1TB of cloud save",
+    active: false,
+    monthlyPrice: 20,
+    yearlyPrice: 120,
+  },
+  {
+    title: "Customizable profile",
+    description: "Custom theme on your profile",
+    active: false,
+    monthlyPrice: 20,
+    yearlyPrice: 150,
+  }
+];
