6ea0f47f560d9283c5cbca3b966e60612e4dea12
Volkan Akyel <volkan@uikielle.com>
2024-03-07 11:41:43 +0100

    gamer-form |📊 SPlit plan to separate component

---
+++ src/components/PlanCard.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="plan-selection">
+    <div class="plan-image">
+      <img :src="dynamicSrc" alt="" />
+    </div>
+    <div class="md:mt-9 text-left">
+      <p class="text-sm text-dark font-semibold">{{ plan.name }}</p>
+      <p class="text-grey-300 mt-2">${{ plan.price }}/mo</p>
+      <p v-show="plan.yearlyPlan" class="text-grey-300 mt-2 text-base">2 months free</p>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed } from "vue";
+import { Plan } from "../../services/formData";
+const props = defineProps<{
+  plan: Plan;
+}>();
+
+const dynamicSrc = computed(() => {
+  try {
+    return `/${props.plan.image}`;
+  } catch (e) {
+    console.error(e);
+    return '';
+  }
+});
+
+const emit = defineEmits(['updatePlanActive']);
+
+</script>
+
+<style scoped></style>
--- src/components/SelectPlanForm.vue
+++ src/components/SelectPlanForm.vue
@@ -5,36 +5,7 @@
       You have the option of monthly or yearly billing.
     </p>
     <div class="mt-8 flex flex-col md:flex-row justify-start gap-5">
-      <div class="plan-selection">
-        <div class="plan-image">
-          <img src="../assets/images/arcade-icon.svg" alt="" />
-        </div>
-        <div class="md:mt-9 text-left">
-          <p class="text-sm text-dark font-semibold">Arcade</p>
-          <p class="text-grey-300 mt-2">$9/mo</p>
-          <p v-show="planStatus" class="text-grey-300 mt-2 text-base">2 months free</p>
-        </div>
-      </div>
-      <div class="plan-selection">
-        <div class="plan-image">
-          <img src="../assets/images/advanced-icon.svg" alt="" />
-        </div>
-        <div class="md:mt-9 text-left">
-          <p class="text-sm text-dark font-semibold">Advanced</p>
-          <p class="text-grey-300 mt-2">$12/mo</p>
-          <p v-show="planStatus" class="text-grey-300 mt-2 text-base">2 months free</p>
-        </div>
-      </div>
-      <div class="plan-selection">
-        <div class="plan-image">
-          <img src="../assets/images/pro-icon.svg" alt="" />
-        </div>
-        <div class="md:mt-9 text-left">
-          <p class="text-sm text-dark font-semibold">Pro</p>
-          <p class="text-grey-300 mt-2">$15/mo</p>
-          <p v-show="planStatus" class="text-grey-300 mt-2 text-base">2 months free</p>
-        </div>
-      </div>
+      <PlanCard v-for="(plan, index) in planSection" :key="index" :plan="plan" />
     </div>
     <PlanComparisonToggle @toggleEvent="updatePlanType" />
   </div>
@@ -42,10 +13,16 @@
 
 <script setup lang="ts">
 import PlanComparisonToggle from './PlanComparisonToggle.vue';
+import { planSelection, Plan } from "../../services/formData"
+import PlanCard from './PlanCard.vue';
 import { ref } from "vue";
-const planStatus = ref<boolean>(false);
-const updatePlanType = (planState: boolean): void => {
-  planStatus.value = planState;
+
+const planSection = ref<Plan[]>(planSelection);
+
+const updatePlanType = (): void => {
+  planSection.value.forEach(plan => {
+    plan.yearlyPlan = !plan.yearlyPlan
+  })
 }
 
 </script>
