d12018ff0c05328e0fb28d60d1b339c73367a2cd
Volkan Akyel <volkan@uikielle.com>
2024-03-09 16:18:43 +0100

    gamer-form | ☄️  add review page depending on plan selected

--- src/components/Review.vue
+++ src/components/Review.vue
@@ -10,44 +10,54 @@
       <!-- Title and Price -->
       <div class="flex justify-between items-center mb-4">
         <div>
-          <h1 class="text-2xl text-dark font-semibold">{{ activePlanName }} ({{ planDuration }})</h1>
+          <h1 class="text-2xl text-dark font-semibold">{{ activePlan?.name }} ({{ planDuration }})</h1>
           <button class="text-blue-500 hover:underline">Change</button>
         </div>
-        <p class="text-2xl font-semibold text-dark">$9/mo</p>
+        <p class="text-2xl font-semibold text-dark">${{ totalPlanPrice }} /mo</p>
       </div>
 
-      <!-- Divider -->
       <div class="border-t border-grey-300 my-4"></div>
 
       <!-- Services List -->
-      <div class="flex justify-between items-center mb-4">
-        <p>Online service</p>
-        <p class="text-dark">+$1/mo</p>
+      <div v-for="(addOn, index) in planAdded" :key="index" class="flex justify-between items-center mb-4">
+        <p>{{ addOn.title }}</p>
+        <p class="text-dark">+${{ yearlyPlan ? addOn.yearlyPrice : addOn.monthlyPrice }} /mo</p>
       </div>
-      <div class="flex justify-between items-center mb-4">
-        <p>Larger storage</p>
-        <p class="text-dark">+$2/mo</p>
-      </div>
-
-      <!-- Total Price -->
     </div>
 
     <div class="flex justify-between items-center mt-6">
       <h2 class="">Total (per month)</h2>
-      <p class="text-2xl font-semibold text-purple">+$12/mo</p>
+      <p class="text-2xl font-semibold text-purple">+${{ totalPrice }}/mo</p>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import { inject, Ref, computed } from "vue";
-import { Plan } from "../../services/formData"
+import { AddOnPlan, Plan } from "../../services/formData"
 
 const planSelection = inject('planSelection') as Plan[];
 const yearlyPlan = inject<Ref<boolean>>('yearlyPlan');
+const addedPlan = inject('addOnsPlan') as AddOnPlan[];
 
-const activePlanName = computed(() => planSelection?.find((plan: Plan) => plan.active)?.name);
+const activePlan = computed(() => planSelection?.find((plan: Plan) => plan.active));
+const planAdded = computed(() => addedPlan.filter((plan: AddOnPlan) => plan.active));
+
 const planDuration = yearlyPlan?.value ? 'Yearly' : 'Monthly';
+
+// Total plan price
+const totalPlanPrice = computed(() => yearlyPlan?.value ? activePlan.value?.yearlyPrice : activePlan.value?.price);
+const totalAddedOnPrice = computed(() => {
+  return planAdded.value.reduce((total, addOn) => {
+    if (addOn.active) {
+      return yearlyPlan?.value ? total + addOn.yearlyPrice : total + addOn.monthlyPrice;
+    }
+    return total;
+  }, 0)
+})
+const totalPrice = computed(() => {
+  return totalPlanPrice?.value + totalAddedOnPrice.value;
+});
 </script>
 
 <style scoped></style>
