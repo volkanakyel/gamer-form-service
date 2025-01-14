06b5812368a8ea113d9b1d798d108933b935d3f1
Volkan AKYEL <volkanakyel@Volkans-Mac.local>
2025-01-14 17:14:28 +0100

    plan card upgrade template

--- src/components/PlanCard.vue
+++ src/components/PlanCard.vue
@@ -1,19 +1,33 @@
 <template>
-  <div @click="selectPlan" :class="plan.active ? 'plan-selection-active' : 'plan-selection-inactive'">
+  <div
+    @click="selectPlan"
+    :class="
+      plan.active
+        ? 'plan-selection-active'
+        : 'plan-selection-inactive'
+    "
+  >
     <div class="plan-image">
       <img :src="dynamicSrc" alt="" />
     </div>
     <div class="md:mt-9 text-left">
-      <p class="text-sm text-dark font-semibold">{{ plan.name }}</p>
+      <p class="text-sm text-dark font-semibold">
+        {{ plan.name }}
+      </p>
       <p class="text-grey-300 mt-2">${{ planPrice }}/mo</p>
-      <p v-show="plan.yearlyPlan" class="text-grey-300 mt-2 text-base">2 months free</p>
+      <p
+        v-show="plan.yearlyPlan"
+        class="text-grey-300 mt-2 text-base"
+      >
+        2 months free
+      </p>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { computed } from "vue";
-import { Plan } from "../../services/formData";
+import { computed } from 'vue';
+import { Plan } from '../../services/formData';
 const props = defineProps<{
   plan: Plan;
 }>();
@@ -27,12 +41,16 @@ const dynamicSrc = computed(() => {
   }
 });
 
-const planPrice = computed(() => props.plan.yearlyPlan ? props.plan.yearlyPrice : props.plan.price)
+const planPrice = computed(() =>
+  props.plan.yearlyPlan
+    ? props.plan.yearlyPrice
+    : props.plan.price
+);
 
 const emit = defineEmits(['selectPlan']);
 const selectPlan = (): void => {
   emit('selectPlan', props.plan.id);
-}
+};
 </script>
 
 <style scoped></style>
